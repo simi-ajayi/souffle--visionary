c714ebaae06138eeb2c774fb959e4b516395bbeb
simi <108358542+simi-ajayi@users.noreply.github.com>
2025-03-08 17:50:27 +0100

    Fix

---
+++ src/components/scriptContent.js
@@ -0,0 +1,86 @@
+export const scriptContent = [
+    {
+      speaker: "MOYIN",
+      text: "(SIGH) Man I'm tired oo, how long have we been here now?"
+    },
+    {
+      speaker: "SDL",
+      text: "(CHECKS TIME) About 2 hours now tbh."
+    },
+    {
+      speaker: "MOYIN",
+      text: "Omo, to blow hard oo."
+    },
+    {
+      speaker: "SDL",
+      text: "(LIGHT CHUCKLE) Maaaannn, e no easy wallahi."
+    },
+    {
+      speaker: "SDL",
+      text: "But back to it though. So we've discussed these ones. (Holding up art prints) What about this one?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "(SIGH) Umm, this one came from like a dark place though."
+    },
+    {
+      speaker: "SDL",
+      text: "Which art doesn't?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "Real though. But this one specifically, cause i think i was in school, and just needed an outlet."
+    },
+    {
+      speaker: "SDL",
+      text: "(NODS) Hmm real, okay. Okay so we've adressed all the art, it's how to incorporate all the concepts that we need to focus on now."
+    },
+    {
+      speaker: "MOYIN",
+      text: "(GROANS) Broo apart from that self, how to adress the issue of niggas mispronouncing my name even."
+    },
+    {
+      speaker: "MOYIN",
+      text: "Cause like, you know it's not just a rebrand, it's also like wanting people to know it's me."
+    },
+    {
+      speaker: "MOYIN",
+      text: "Like they should see the art and immedietly be able to like identify that it's me, do you get?"
+    },
+    {
+      speaker: "SDL",
+      text: "(NODS) Okay i get you, but what's the issue with your name though?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "Okay, so now it's like, how tf am i supposed to create that connection when most people, see my page and think \"soulful\" instead of \"souffle\""
+    },
+    {
+      speaker: "SDL",
+      text: "People actually mistake it for soulful?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "(SLIGHT ANNOYANCE) Yes!!!"
+    },
+    {
+      speaker: "MOYIN",
+      text: "Guy like it's actually a thing, i could meet a random person now yeah? and we're chopping it up right?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "Okay we're talking and all, then it'll move to talking about what we do, and I'll bring up my art and all."
+    },
+    {
+      speaker: "MOYIN",
+      text: "Okay now, I'll show them my page and they fuck with the art and all, then they'll just say, \"oh your name is soulful\" guyy, and my heart will just die."
+    },
+    {
+      speaker: "RANDOM",
+      text: "Soulful?"
+    },
+    {
+      speaker: "MOYIN",
+      text: "(ANNOYED) It's fuckin soufflé."
+    }
+  ];
