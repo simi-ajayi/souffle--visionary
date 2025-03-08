1b6d2c97818e9d1422ccfe1a54346a575d961a04
simi <108358542+simi-ajayi@users.noreply.github.com>
2025-03-08 11:36:14 +0100

    Work

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AboutArtist from './pages/AboutArtist';
 
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import ArtworkShowcase from './components/Home';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
